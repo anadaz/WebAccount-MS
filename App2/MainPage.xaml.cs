1616372cfa072d60c00a625b2cbf4f1149ffa324
Ahmed Nada <ahmed.nada@hotmail.com>
2016-06-07 13:20:03 +1000

    intial commit

---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Web.Core;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            AccountsSettingsPane.Show();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            AccountsSettingsPane.GetForCurrentView().AccountCommandsRequested += BuildPaneAsync;
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            AccountsSettingsPane.GetForCurrentView().AccountCommandsRequested -= BuildPaneAsync;
+        }
+        private async void BuildPaneAsync(AccountsSettingsPane s,
+    AccountsSettingsPaneCommandsRequestedEventArgs e)
+        {
+            var deferral = e.GetDeferral();
+            var msaProvider = await WebAuthenticationCoreManager.FindAccountProviderAsync(
+        "https://login.microsoft.com", "consumers");
+            var command = new WebAccountProviderCommand(msaProvider, GetMsaTokenAsync);
+
+            e.WebAccountProviderCommands.Add(command);
+            deferral.Complete();
+        }
+        private async void GetMsaTokenAsync(WebAccountProviderCommand command)
+        {
+           
+            WebTokenRequest request = new WebTokenRequest(command.WebAccountProvider, "wl.basic");
+            WebTokenRequestResult result = await WebAuthenticationCoreManager.RequestTokenAsync(request);
+            if (result.ResponseStatus == WebTokenRequestStatus.Success)
+            {
+                string token = result.ResponseData[0].Token;
+            }
+        }
+    }
+}
